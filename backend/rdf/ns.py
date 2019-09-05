2bcf77c004a6f3fbcef03fce9e4f740e3cddd315
Julian Gonggrijp <j.gonggrijp@uu.nl>
2019-09-05 15:31:31 +0200

    Add the backend.rdf.ns module (#80, #81, #83)

---
+++ backend/rdf/ns.py
@@ -0,0 +1,9 @@
+""" External namespaces that we use internally. """
+
+from rdflib.namespace import Namespace, RDF, RDFS, OWL, XSD, FOAF, SKOS, DC, DCTERMS
+
+FRBR    = Namespace('http://purl.org/vocab/frbr/core#')
+OA      = Namespace('http://www.w3.org/ns/oa#')
+AS      = Namespace('http://www.w3.org/ns/activitystreams#')
+DCTYPES = Namespace('http://purl.org/dc/dcmitype/')
+SCHEMA  = Namespace('http://schema.org/')
