6b02cedf2b6f2f80eac6431289863269172161d5
Julian Gonggrijp <dev@juliangonggrijp.com>
2020-10-01 15:13:03 +0200

    Restore Python 3.4 compatibility (#89 #358)

--- backend/sparql/urls.py
+++ backend/sparql/urls.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from .endpoints.items import ITEMS_URLS
@@ -6,10 +8,10 @@ from .endpoints.ontology import ONTOLOGY_URLS
 from .endpoints.sources import SOURCES_URLS
 from .endpoints.vocab import VOCAB_URLS
 
-urlpatterns = format_suffix_patterns([
-    *ITEMS_URLS,
-    *NLP_ONTOLOGY_URLS,
-    *ONTOLOGY_URLS,
-    *SOURCES_URLS,
-    *VOCAB_URLS
-])
+urlpatterns = format_suffix_patterns(chain(
+    ITEMS_URLS,
+    NLP_ONTOLOGY_URLS,
+    ONTOLOGY_URLS,
+    SOURCES_URLS,
+    VOCAB_URLS,
+))
