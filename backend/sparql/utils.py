b0fe456516f23d19fde1614626415cb908e31198
Jelte van Boheemen <j.vanboheemen@uu.nl>
2021-03-26 13:58:41 +0100

    Some more xml sanitize testing

--- backend/sparql/utils.py
+++ backend/sparql/utils.py
@@ -20,3 +20,23 @@ def invalid_xml_remove(c):
 
     illegal_xml_re = re.compile(u'[%s]' % u''.join(illegal_ranges))
     return re.sub(illegal_xml_re, ' ', c)
+
+
+def find_invalid_xml(c):
+    # via http://stackoverflow.com/questions/1707890/fast-way-to-filter-illegal-xml-unicode-chars-in-python
+    illegal_unichrs = [
+        (0x00, 0x08), (0x0B, 0x1F), (0x7F, 0x84), (0x86, 0x9F),
+        (0xD800, 0xDFFF), (0xFDD0, 0xFDDF), (0xFFFE, 0xFFFF),
+        (0x1FFFE, 0x1FFFF), (0x2FFFE, 0x2FFFF), (0x3FFFE, 0x3FFFF),
+        (0x4FFFE, 0x4FFFF), (0x5FFFE, 0x5FFFF), (0x6FFFE, 0x6FFFF),
+        (0x7FFFE, 0x7FFFF), (0x8FFFE, 0x8FFFF), (0x9FFFE, 0x9FFFF),
+        (0xAFFFE, 0xAFFFF), (0xBFFFE, 0xBFFFF), (0xCFFFE, 0xCFFFF),
+        (0xDFFFE, 0xDFFFF), (0xEFFFE, 0xEFFFF), (0xFFFFE, 0xFFFFF),
+        (0x10FFFE, 0x10FFFF)
+    ]
+    illegal_ranges = ["%s-%s" % (chr(low), chr(high))
+                      for (low, high) in illegal_unichrs
+                      if low < sys.maxunicode]
+
+    illegal_xml_re = re.compile(u'[%s]' % u''.join(illegal_ranges))
+    return re.search(illegal_xml_re, c)
--- backend/sparql/utils_test.py
+++ backend/sparql/utils_test.py
@@ -1,9 +1,32 @@
 from .utils import invalid_xml_remove
+import os.path as op
 
+HERE = op.dirname(op.abspath(__file__))
 INVALID_STR = '''La trilogie a en eet
 comptabilisé plus de 26 millions de vente.'''
 VALID_STR = '''La trilogie a en e et
 comptabilisé plus de 26 millions de vente.'''
+
+FULL_STR = """Babelio, Missbouquin (id_12_a) --- Et voilà ça y est ! j'ai enfin lu le premier tome de Millenium ! Harcelée
+par deux de mes meilleures amies (l'une a fini par me l'offrir), j'ai pris
+mon courage à deux mains et me suis attaquée à la série qui a connu
+un succès immense depuis sa publication en 2005. La trilogie a en eet
+comptabilisé plus de 26 millions de vente.
+Pour moi, ce n'était pas gagné d'avance. Je l'ai commencé avec réti-
+cence car je n'aime pas le genre du roman policier / roman noir. Je suis
+trop sensible et assez impressionnable donc je m'en passe très bien.
+On m'a cependant assuré que Millenium allait me plaire, etc etc. Bien.
+Alors je le commence le mardi matin dans le RER. Je le lis tranquille,
+j'accroche bien. Mardi passe. Mercredi, je rentre plus tôt d'une formation
+et comme je le fais parfois, je continue ma lecture en marchant (non non
+ne vous inquiétez pas, il ne va rien m'arriver). Incapable de décrocher, je
+me laisse 30 minutes le temps de boire un thé, en continuant ma lecture.
+Il était 18h. Finalement, je repose le bouquin à 21H30. Terminé.
+C'est un véritable page-turner comme on appelle ces romans qui sont à
+la limite de l'addiction, tant une page appelle la suivante, tant l'histoire
+est prenante. Effectivement, j'ai été impressionnée par l'ampleur de ce
+roman assez complexe, mêlant crimes et arnaques financières, avec au
+centre un journaliste et une jeune femme sous tutelle et assez...spéciale"""
 
 
 def test_xml_sanitize():
@@ -11,3 +34,16 @@ def test_xml_sanitize():
     assert INVALID_STR[18] == chr(0x1b) == '\x1b'
     sanitized_str = invalid_xml_remove(INVALID_STR)
     assert sanitized_str[18] == chr(0x20) == ' '
+    print(len(FULL_STR), len(invalid_xml_remove(FULL_STR)))
+    assert FULL_STR == invalid_xml_remove(FULL_STR)
+
+
+def test_sanitize_file():
+    fp1 = op.join(HERE, '1.txt')
+
+    with open(fp1, 'r') as f1:
+        c1 = f1.read()
+        c1_san = invalid_xml_remove(c1)
+        assert c1[319] == chr(0x1b) == '\x1b'
+        assert c1_san[319] == chr(0x20) == ' '
+        assert len(c1) == len(c1_san)
