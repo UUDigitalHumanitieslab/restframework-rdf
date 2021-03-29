646fa2044be483a5fffb58a79f177dad83a6739d
Jelte van Boheemen <j.vanboheemen@uu.nl>
2021-03-29 11:19:03 +0200

    Fix language tags, cleanup

--- backend/sparql/utils.py
+++ backend/sparql/utils.py
@@ -2,45 +2,29 @@ import sys
 import re
 from rdflib import Literal
 
+ILLEGAL_UNICHRS = [
+        (0x00, 0x08), (0x0B, 0x1F), (0x7F, 0x84), (0x86, 0x9F),
+        (0xD800, 0xDFFF), (0xFDD0, 0xFDDF), (0xFFFE, 0xFFFF),
+        (0x1FFFE, 0x1FFFF), (0x2FFFE, 0x2FFFF), (0x3FFFE, 0x3FFFF),
+        (0x4FFFE, 0x4FFFF), (0x5FFFE, 0x5FFFF), (0x6FFFE, 0x6FFFF),
+        (0x7FFFE, 0x7FFFF), (0x8FFFE, 0x8FFFF), (0x9FFFE, 0x9FFFF),
+        (0xAFFFE, 0xAFFFF), (0xBFFFE, 0xBFFFF), (0xCFFFE, 0xCFFFF),
+        (0xDFFFE, 0xDFFFF), (0xEFFFE, 0xEFFFF), (0xFFFFE, 0xFFFFF),
+        (0x10FFFE, 0x10FFFF)
+    ]
+ILLEGAL_RANGES = ["%s-%s" % (chr(low), chr(high))
+                  for (low, high) in ILLEGAL_UNICHRS if low < sys.maxunicode]
+
+ILLEGAL_XML_RE = re.compile(u'[%s]' % u''.join(ILLEGAL_RANGES))
+
 
 def invalid_xml_remove(c):
     # via http://stackoverflow.com/questions/1707890/fast-way-to-filter-illegal-xml-unicode-chars-in-python
-    illegal_unichrs = [
-        (0x00, 0x08), (0x0B, 0x1F), (0x7F, 0x84), (0x86, 0x9F),
-        (0xD800, 0xDFFF), (0xFDD0, 0xFDDF), (0xFFFE, 0xFFFF),
-        (0x1FFFE, 0x1FFFF), (0x2FFFE, 0x2FFFF), (0x3FFFE, 0x3FFFF),
-        (0x4FFFE, 0x4FFFF), (0x5FFFE, 0x5FFFF), (0x6FFFE, 0x6FFFF),
-        (0x7FFFE, 0x7FFFF), (0x8FFFE, 0x8FFFF), (0x9FFFE, 0x9FFFF),
-        (0xAFFFE, 0xAFFFF), (0xBFFFE, 0xBFFFF), (0xCFFFE, 0xCFFFF),
-        (0xDFFFE, 0xDFFFF), (0xEFFFE, 0xEFFFF), (0xFFFFE, 0xFFFFF),
-        (0x10FFFE, 0x10FFFF)
-    ]
-    illegal_ranges = ["%s-%s" % (chr(low), chr(high))
-                      for (low, high) in illegal_unichrs
-                      if low < sys.maxunicode]
-
-    illegal_xml_re = re.compile(u'[%s]' % u''.join(illegal_ranges))
-    return re.sub(illegal_xml_re, ' ', c)
+    return re.sub(ILLEGAL_XML_RE, ' ', c)
 
 
 def find_invalid_xml(c):
-    # via http://stackoverflow.com/questions/1707890/fast-way-to-filter-illegal-xml-unicode-chars-in-python
-    illegal_unichrs = [
-        (0x00, 0x08), (0x0B, 0x1F), (0x7F, 0x84), (0x86, 0x9F),
-        (0xD800, 0xDFFF), (0xFDD0, 0xFDDF), (0xFFFE, 0xFFFF),
-        (0x1FFFE, 0x1FFFF), (0x2FFFE, 0x2FFFF), (0x3FFFE, 0x3FFFF),
-        (0x4FFFE, 0x4FFFF), (0x5FFFE, 0x5FFFF), (0x6FFFE, 0x6FFFF),
-        (0x7FFFE, 0x7FFFF), (0x8FFFE, 0x8FFFF), (0x9FFFE, 0x9FFFF),
-        (0xAFFFE, 0xAFFFF), (0xBFFFE, 0xBFFFF), (0xCFFFE, 0xCFFFF),
-        (0xDFFFE, 0xDFFFF), (0xEFFFE, 0xEFFFF), (0xFFFFE, 0xFFFFF),
-        (0x10FFFE, 0x10FFFF)
-    ]
-    illegal_ranges = ["%s-%s" % (chr(low), chr(high))
-                      for (low, high) in illegal_unichrs
-                      if low < sys.maxunicode]
-
-    illegal_xml_re = re.compile(u'[%s]' % u''.join(illegal_ranges))
-    return re.search(illegal_xml_re, c)
+    return re.search(ILLEGAL_XML_RE, c)
 
 
 def is_cleanable(term):
@@ -50,15 +34,12 @@ def is_cleanable(term):
 def clean_term(term):
     if not is_cleanable(term):
         return term
-    return Literal(invalid_xml_remove(term))
+    return Literal(invalid_xml_remove(term), lang=term.language)
 
 
 def xml_sanitize_triple(triple):
-    if any((isinstance(term, Literal) and term.datatype is None) for term in triple):
-        cleaned_terms = []
-        for term in triple:
-            cleaned_terms.append(clean_term(term))
-        cleaned_triple = tuple(cleaned_terms)
+    if any((is_cleanable(term)) for term in triple):
+        cleaned_triple = tuple([clean_term(t) for t in triple])
         if cleaned_triple != triple:
             return (True, cleaned_triple)
     return (False, triple)
