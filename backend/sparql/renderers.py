b5307e759e2a107dab79f1d40aee0d026e9966e3
Jelte van Boheemen <j.vanboheemen@uu.nl>
2021-06-22 16:57:38 +0200

    Manual error when serializing results fail
    Resolves #394

--- backend/sparql/renderers.py
+++ backend/sparql/renderers.py
@@ -1,18 +1,33 @@
 import json
 from io import StringIO
 
+from rdf.ns import HTTP, HTTPSC, RDF
+from rdf.renderers import RDFLibRenderer, TurtleRenderer
+from rdf.utils import graph_from_triples
+from rdflib import BNode, Literal
 from rdflib.plugins.sparql.results.jsonresults import JSONResultSerializer
 from rest_framework.renderers import JSONRenderer
-
-from rdf.renderers import TurtleRenderer, RDFLibRenderer
-from rdf.utils import graph_from_triples
 
 
 class QueryResultsTurtleRenderer(TurtleRenderer):
     ''' Renders turtle from rdflib SPARQL query results'''
 
     def render(self, query_results, media_type=None, renderer_context=None):
-        results_graph = graph_from_triples(query_results)
+        try:
+            results_graph = graph_from_triples(query_results)
+        except Exception as e:
+            ''' Hacky solution, because we are beyond generating the response.
+            Manually create an error response '''
+            blank = BNode()
+            results_graph = graph_from_triples(
+                (
+                    (blank, RDF.type, HTTP.Response),
+                    (blank, HTTP.statusCodeValue, Literal(400)),
+                    (blank, HTTP.reasonPhrase, Literal(str(e))),
+                    (blank, HTTP.sc, HTTPSC.BadRequest),
+                )
+            )
+            renderer_context['response'].status_code = 400
         return super().render(results_graph, media_type, renderer_context)
 
 
--- backend/sparql/views_test.py
+++ backend/sparql/views_test.py
@@ -35,7 +35,6 @@ def test_ask(client, test_queries, ontologygraph_db):
     true_response = client.get(
         QUERY_URL, {'query': test_queries.ASK_TRUE})
     assert true_response.status_code == 200
-    print(true_response.content)
     assert json.loads(true_response.content.decode('utf8'))['boolean']
     assert check_content_type(true_response, 'application/sparql-results+json')
 
