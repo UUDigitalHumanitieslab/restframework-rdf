5ebc6306e7ce07842b35d7b20918b436650096d3
Julian Gonggrijp <dev@juliangonggrijp.com>
2021-10-27 15:55:11 +0200

    Spam the backend with logging statements for debugging purposes

--- backend/sparql/endpoints/nlp_ontology.py
+++ backend/sparql/endpoints/nlp_ontology.py
@@ -1,8 +1,12 @@
+import logging
+
 from django.urls import path
 
 from nlp_ontology import NLP_ONTOLOGY_ROUTE
 from nlp_ontology.graph import graph
 from sparql.views import SPARQLQueryAPIView, SPARQLUpdateAPIView
+
+logger = logging.getLogger(__name__)
 
 
 class NlpOntologyQueryView(SPARQLQueryAPIView):
@@ -14,6 +18,7 @@ class NlpOntologyQueryView(SPARQLQueryAPIView):
 class NlpOntologyUpdateView(SPARQLUpdateAPIView):
 
     def graph(self):
+        logger.debug('graph accessed in NlpOntologyUpdateView')
         return graph()
 
 
--- backend/sparql/views.py
+++ backend/sparql/views.py
@@ -1,4 +1,5 @@
 import re
+import logging
 
 from django.http.response import HttpResponseBase
 from pyparsing import ParseException
@@ -20,6 +21,8 @@ from .exceptions import (BlankNodeError, NoParamError, ParseSPARQLError,
 from .negotiation import SPARQLContentNegotiator
 from .permissions import SPARQLPermission
 
+logger = logging.getLogger(__name__)
+
 
 class SPARQLUpdateAPIView(APIView):
     renderer_classes = (TurtleRenderer,)
@@ -29,18 +32,22 @@ class SPARQLUpdateAPIView(APIView):
         return turtle_exception_handler
 
     def check_supported(self, updatestring):
+        logger.debug('enter SPARQLUpdateAPIView.check_supported')
         # check the entire string for unsupported keywords
         if re.match(UPDATE_NOT_SUPPORTED_PATTERN, updatestring):
+            logger.debug('UPDATE_NOT_SUPPORTED_PATTERN matched')
             # do a dry-run parse of the updatestring
             parse_request = parseUpdate(updatestring).request
             # check if the parse contains any unsupported operations
             if any(part.name in UPDATE_NOT_SUPPORTED for part in parse_request):
+                logger.debug('unsupported operation confirmed')
                 raise UnsupportedUpdateError(
                     'Update operation is not supported.'
                 )
 
         # Do a quick check for blank nodes
         if re.search(BLANK_NODE_PATTERN, updatestring):
+            logger.debug('BLANK_NODE_PATTERN matched')
             parse_request = parseUpdate(updatestring).request
             for part in parse_request:
                 if any(
@@ -48,38 +55,51 @@ class SPARQLUpdateAPIView(APIView):
                         for triple in part.quads.triples
                         for term in triple
                 ):
+                    logger.debug('blank node confirmed')
                     raise BlankNodeError()
 
+        logger.debug('exit SPARQLUpdateAPIView.check_supported')
         return
 
     def execute_update(self, updatestring):
+        logger.debug('enter SPARQLUpdateAPIView.execute_update')
         graph = self.graph()
 
         try:
             self.check_supported(updatestring)
+            logger.debug('graph size before update: %d', len(graph))
             graph.update(updatestring)
+            logger.debug('graph size after update: %d', len(graph))
         except (ParseException, ParseError) as p_e:
             # Raised when SPARQL syntax is not valid, or parsing fails
             graph.rollback()
+            logger.exception('parse error')
             raise ParseSPARQLError(p_e)
         except HTTPError as h_e:
             graph.rollback()
             if 400 <= h_e.response.status_code < 500:
+                logger.exception('parse error on remote server')
                 raise ParseSPARQLError(h_e)
             else:
+                logger.exception('internal error on remote server')
                 raise APIException(h_e)
         except Exception as e:
             graph.rollback()
+            logger.exception('another error')
             raise APIException(e)
+        logger.debug('exit SPARQLUpdateAPIView.execute_update')
 
     def post(self, request, **kwargs):
         """ Accepts POST request with SPARQL-Query in body parameter 'query'
             Renders text/turtle
         """
+        logger.debug('enter SPARQLUpdateAPIView.post (%s)', request.build_absolute_uri())
         sparql_string = request.data.get("update")
         if not sparql_string:
             # POST must contain an update
+            logger.debug('no query update string')
             raise NoParamError()
+        logger.debug('query update string:\n %s', sparql_string)
         blank = BNode()
         status = 200
         response = graph_from_triples(
@@ -91,7 +111,7 @@ class SPARQLUpdateAPIView(APIView):
             )
         )
         self.execute_update(sparql_string)
-
+        logger.debug('returning from SPARQLUpdateAPIView.post')
         return Response(response)
 
     def graph(self):
@@ -125,7 +145,7 @@ class SPARQLQueryAPIView(APIView):
                 query_results = graph.query(querystring)
                 query_type = query_results.type
             self.request.data["query_type"] = query_type
-            
+
             # re-perform content negotiation to determine if
             # querytype satisfies accept header
             neg = self.perform_content_negotiation(self.request)
