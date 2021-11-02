f94922249f760b2b0a8c5c9042c18d299b25cc1b
Julian Gonggrijp <dev@juliangonggrijp.com>
2021-11-02 15:04:49 +0100

    Revert "Spam the backend with logging statements for debugging purposes"
    This reverts commit 5ebc6306e7ce07842b35d7b20918b436650096d3.

--- backend/sparql/endpoints/nlp_ontology.py
+++ backend/sparql/endpoints/nlp_ontology.py
@@ -1,12 +1,8 @@
-import logging
-
 from django.urls import path
 
 from nlp_ontology import NLP_ONTOLOGY_ROUTE
 from nlp_ontology.graph import graph
 from sparql.views import SPARQLQueryAPIView, SPARQLUpdateAPIView
-
-logger = logging.getLogger(__name__)
 
 
 class NlpOntologyQueryView(SPARQLQueryAPIView):
@@ -18,7 +14,6 @@ class NlpOntologyQueryView(SPARQLQueryAPIView):
 class NlpOntologyUpdateView(SPARQLUpdateAPIView):
 
     def graph(self):
-        logger.debug('graph accessed in NlpOntologyUpdateView')
         return graph()
 
 
