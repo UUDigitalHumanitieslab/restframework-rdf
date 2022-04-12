f37bef69b2ba9b7aaf43f2f1aeecda0edbbce97a
Jelte van Boheemen <j.vanboheemen@uu.nl>
2022-04-12 12:08:20 +0200

    Retain initNs when injecting prefixes

--- backend/rdf/utils.py
+++ backend/rdf/utils.py
@@ -9,6 +9,8 @@ from rdflib import ConjunctiveGraph, Graph, Literal, URIRef
 from rdflib.plugins.stores.sparqlstore import SPARQLStore
 from rdflib_django.models import Store
 
+PREFIX_PATTERN = re.compile(r'PREFIX\s+(\w+):\s*<\S+>', re.IGNORECASE)
+
 
 def get_conjunctive_graph():
     """ Returns the conjunctive graph of our SPARQL store. """
@@ -21,16 +23,18 @@ def prune_triples(graph, triples):
         graph.remove(triple)
 
 
-def prune_triples_cascade(graph, triples, graphs_applied_to = [], privileged_predicates = []):
+def prune_triples_cascade(graph, triples, graphs_applied_to=[], privileged_predicates=[]):
     """
     Recursively remove subjects in `triples` and all related resources from `graph`.
     Specify which graphs qualify, i.e. from which triples will be deleted, in `graphs_applied_to`.
     Optionally, skip items related via specific (privileged) predicates.
     """
     for triple in triples:
-        prune_recursively(graph, triple[0], graphs_applied_to, privileged_predicates)
+        prune_recursively(
+            graph, triple[0], graphs_applied_to, privileged_predicates)
 
-def prune_recursively(graph, subject, graphs_applied_to = [], privileged_predicates = []):
+
+def prune_recursively(graph, subject, graphs_applied_to=[], privileged_predicates=[]):
     """
     Recursively remove subject and all related resources from `graph`.
     Specify which graphs qualify, i.e. from which triples will be deleted, in `graphs_applied_to`.
@@ -40,7 +44,8 @@ def prune_recursively(graph, subject, graphs_applied_to = [], privileged_predica
 
     for s, p, o, c in related_by_subject:
         if isinstance(o, URIRef) and o != s and p not in privileged_predicates and c in graphs_applied_to:
-            prune_recursively(graph, o, graphs_applied_to, privileged_predicates)
+            prune_recursively(graph, o, graphs_applied_to,
+                              privileged_predicates)
 
     prune_triples(graph, related_by_subject)
 
@@ -61,7 +66,7 @@ def graph_from_triples(triples, ctor=Graph):
 def sample_graph(graph, subjects, request):
     """ Return a random sample from a graph, optionally filtering with a list containing [predicate, object]. """
     n_results = int(request.GET.get('n_results'))
-    if len(subjects)>n_results:
+    if len(subjects) > n_results:
         sampled_subjects = random.sample(list(subjects), n_results)
     else:
         sampled_subjects = subjects
@@ -70,6 +75,7 @@ def sample_graph(graph, subjects, request):
         suggestions = graph.triples((sub, None, None))
         [output.add(s) for s in suggestions]
     return output
+
 
 def traverse_forward(full_graph, fringe, plys):
     """
@@ -124,18 +130,26 @@ def traverse_backward(full_graph, fringe, plys):
 
 
 def patched_inject_prefixes(self, query, extra_bindings):
-    ''' Monkeypatch for SPARQLStore prefix bindings
+    ''' Monkeypatch for SPARQLStore prefix injection
     Parses the incoming query for prefixes,
     and ignores these when injecting additional namespaces.
     Better implementation is possibly available,
     e.g. use rdfblibs query parser to extract prefixes.
     '''
-    prefix_pattern = re.compile(r'PREFIX\s+(\w+):\s*<\S+>', re.IGNORECASE)
-    query_prefixes = re.findall(prefix_pattern, query)
+    query_prefixes = re.findall(PREFIX_PATTERN, query)
 
-    bindings = set(list(self.nsBindings.items()) +
-                   list(extra_bindings.items()))
-    bindings = {x for x in bindings if x[0] not in query_prefixes}
+    # prefixes available in the query should be deducted from the store's nsBindings
+    # prefixes that were provided through initNs should take precedence over all others
+    bindings = {x for x in set(self.nsBindings.items())
+                if x[0] not in query_prefixes}
+    bindings |= set(extra_bindings.items())
+
+    # remove the extra bindings from the original query
+    for k in set(extra_bindings.keys()):
+        if k in query_prefixes:
+            replace_pattern = re.compile(
+                fr'PREFIX\s+{k}:\s*<.+>', re.IGNORECASE)
+            query = re.sub(replace_pattern, '', query)
 
     if not bindings:
         return query
